f04ad4c8cb41c3860078e49850fa64e89d2d79e4
Walt <waltpro.trader@gmail.com>
2023-11-06 15:24:07 -0300

    declaring variable

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-  int file_size = 100;
+  int file_size = 0;
+  int counter = 0;
   double sales = 9.99;
 
-  std::cout << sales;
+  cout << file_size;
+  cout << sales;
   return 0;
 
 }
